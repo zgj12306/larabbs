4ebc91e8c8c06dc24dadf106e42a8a6d97c1977d
zgj12306 <2331766479@qq.com>
2020-05-25 21:21:38 +0800

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+
+    // 随机生成一个月内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
